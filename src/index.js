8e5bdea7c9aaceb98f42635c160a69159ea33578
WagaoFC <wagnerferreira_07@hotmail.com>
2022-04-01 06:57:26 -0300

    iniciando FinApi

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/courses', (request, response) => {
-    const query = request.query;
-    console.log(query);
-    return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
-});
+const customers = [];
 
-app.post('/courses', (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
-});
+app.post('/account', (resquest, reponse) => {
+    const { cpf, name } = resquest.body;
 
-app.put('/courses/:id', (request, response) => {
-    const { id } = request.params;
-    console.log(id);
-    return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
-});
+    const id = uuidv4();
 
-app.patch('/courses/:id', (request, response) => {
-    return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
-});
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: [],
+    });
 
-app.delete('/courses/:id', (request, response) => {
-    return response.json(['Curso 6', 'Curso2', 'Curso 4']);
+    return reponse.status(201).send();
 });
 
 app.listen(3333);
